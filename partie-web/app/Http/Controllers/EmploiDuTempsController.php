9d1468ec42608efe298346ab9df92d3e86ba185d
AhmedBelkadi <ahmad.belkadi12@gmail.com>
2024-02-16 18:57:56 +0100

    16/02 m3a 18:57

--- partie-web/app/Http/Controllers/EmploiDuTempsController.php
+++ partie-web/app/Http/Controllers/EmploiDuTempsController.php
@@ -30,15 +30,15 @@ class EmploiDuTempsController extends Controller
 
     public function index()
     {
+
+        $semestres = Semestre::all();
+        $filieres = Filiere::all();
+        $salles = Salle::all();
+
         $periodes = Periode::all();
         $days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-
-        // Fetch all emploi_du_temps records along with related filiere and semestre
         $emploises = EmploiDuTemps::with(['filiere', 'semestre', 'seances'])->paginate(1);
-
-        // Organize the schedule data
         $schedule = [];
-
         foreach ($emploises as $emploi) {
 //            dd($schedule[$emploi->filiere->name][$emploi->semestre->name]);
 
@@ -60,10 +60,7 @@ class EmploiDuTempsController extends Controller
                 }
             }
         }
-//        dd($schedule);
-
-        // Pass the data to the view
-        return view("admin.emplois.index", compact("periodes", "days", "schedule", "emploises"));
+        return view("admin.emplois.index", compact("periodes", "days", "schedule", "emploises","salles","filieres","semestres"));
     }
 
 
@@ -86,9 +83,31 @@ class EmploiDuTempsController extends Controller
         $filieres = Filiere::all();
         $salles = Salle::all();
         $periodes = Periode::all();
-//        $emploises = EmploiDuTemps::paginate(1);
         $days = ["Monday","Tuesday","Wednesday","Thursday","Friday"];
-        return view("admin.emplois.index" ,compact("semestres","filieres","salles","periodes","days","emploises") );
+        $schedule = [];
+        foreach ($emploises as $emploi) {
+//            dd($schedule[$emploi->filiere->name][$emploi->semestre->name]);
+
+            $schedule[$emploi->filiere->name][$emploi->semestre->name] = [];
+
+            foreach ($days as $day) {
+//                dd($schedule[$emploi->filiere->name][$emploi->semestre->name][$day] );
+                $schedule[$emploi->filiere->name][$emploi->semestre->name][$day] = [];
+
+                foreach ($periodes as $periode) {
+                    $schedule[$emploi->filiere->name][$emploi->semestre->name][$day][$periode->id] = [];
+
+                    // Fill in the sessions for the day and period
+                    foreach ($emploi->seances as $seance) {
+                        if ($seance->day == $day && $seance->id_periode == $periode->id) {
+                            $schedule[$emploi->filiere->name][$emploi->semestre->name][$day][$periode->id][] = $seance;
+                        }
+                    }
+                }
+            }
+        }
+
+        return view("admin.emplois.index" ,compact("semestres","filieres","salles","periodes","days","emploises","schedule") );
 
     }
 
